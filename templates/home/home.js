57af38bda08892f7336fffa18a41bb33dc007d38
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2025-12-27 10:26:58 +0530

    Add files via upload

--- templates/home/home.js
+++ templates/home/home.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-let merge_pdf = document.getElementById("merge_pdf");
-let docx_pdf = document.getElementById("docx_pdf");
-let image_pdf = document.getElementById("image_pdf");
-let ppt_pdf = document.getElementById("ppt_pdf");
-let pdf_png = document.getElementById("pdf_png");
-let pdf_docx = document.getElementById("pdf_docx");
+document.addEventListener("DOMContentLoaded", function () {
+    let merge_pdf = document.getElementById("merge_pdf");
+    let docx_pdf = document.getElementById("docx_pdf");
+    let image_pdf = document.getElementById("image_pdf");
+    let ppt_pdf = document.getElementById("ppt_pdf");
+    let pdf_png = document.getElementById("pdf_png");
+    let excel_pdf = document.getElementById("excel_pdf");
 
 
-const url = "http://localhost:3000";
+    const url = "http://localhost:3000";
 
-merge_pdf.addEventListener("click",function(){
-    window.location.href=`${url}/merge_pdf`;
-})
-docx_pdf.addEventListener("click",function(){
-    window.location.href=`${url}/docx_pdf`;
-})
-image_pdf.addEventListener("click",function(){
-    window.location.href=`${url}/image_pdf`;
-})
-ppt_pdf.addEventListener("click",function(){
-    window.location.href=`${url}/ppt_pdf`;
-})
-pdf_png.addEventListener("click",function(){
-    window.location.href=`${url}/pdf_png`;
-})
-pdf_docx.addEventListener("click",function(){
-    window.location.href=`${url}/pdf_docx`;
-})
+    merge_pdf.addEventListener("click", function () {
+        window.location.href = `${url}/merge_pdf`;
+    })
+    docx_pdf.addEventListener("click", function () {
+        window.location.href = `${url}/docx_pdf`;
+    })
+    image_pdf.addEventListener("click", function () {
+        window.location.href = `${url}/image_pdf`;
+    })
+    ppt_pdf.addEventListener("click", function () {
+        window.location.href = `${url}/ppt_pdf`;
+    })
+    pdf_png.addEventListener("click", function () {
+        window.location.href = `${url}/pdf_png`;
+    })
+    excel_pdf.addEventListener("click", function () {
+        window.location.href = `${url}/excel_pdf`;
+    })
 
 })
