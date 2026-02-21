e421007da59fb817c381c0b483b8e543b0db6cb9
GoyaniJay1905 <gc2xnzs7md@privaterelay.appleid.com>
2026-02-21 08:59:37 +0530

    Add files via upload

--- React-pdf-tools/src/App.jsx
+++ React-pdf-tools/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ToolUpload from "./pages/ToolUpload";
 import ToolSplit from "./pages/ToolSplit";
-
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/tool/:toolName" element={<ToolUpload />} />
         <Route path="/tool/split-pdf" element={<ToolSplit />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
