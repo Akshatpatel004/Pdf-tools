87188a5b9aef826b90e4c030af589d934509a9e8
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2026-02-22 18:31:08 +0530

    Add files via upload

--- React-pdf-tools/src/App.jsx
+++ React-pdf-tools/src/App.jsx
@@ -1,17 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import ToolUpload from "./pages/ToolUpload";
 import ToolSplit from "./pages/ToolSplit";
-import Login from "./pages/Login";
+
+import AiChatBot from "./component/AiChatBot";
+
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/tool/:toolName" element={<ToolUpload />} />
         <Route path="/tool/split-pdf" element={<ToolSplit />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
+
+      {/* Floating chatbot */}
+      <AiChatBot />
+
     </BrowserRouter>
   );
 }
