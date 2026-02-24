23d6892f938e6c4251a286e977bc6459b13b4eba
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2026-02-24 14:34:15 +0530

    Update server.js

--- PDF-TOOLS_Telegram-bot/server.js
+++ PDF-TOOLS_Telegram-bot/server.js
@@ -339,10 +339,9 @@ app.post('/split-pdf', upload.any(), async (req, res) => {
 
 
 
-
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-// require('./client_bot.js');
+require('./client_bot.js');
+
