48595cc2c94ef6efd8acff14915c5aa26be24c41
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2026-04-12 19:27:58 +0530

    Add files via upload

--- PDF-TOOLS_Telegram-bot/routes/imageEditor.js
+++ PDF-TOOLS_Telegram-bot/routes/imageEditor.js
@@ -8,13 +8,16 @@ const util = require('util');
 const execPromise = util.promisify(exec);
 const { createZipFile, cleanupFiles } = require('../utils/utils'); 
 
+const sharp = require('sharp');
+
 const upload = multer({ dest: "Uploads/" });
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 const main_dir = [
-  "Download/bgImageRemoval/",
-  "Download/temp_pdf/",
+  "Download/temp_pdf/",           // index 0
+  "Download/compressImageSize/",  // index 1
+  "Download/imageFormatConverter/",  // index 2
 ];
 
 function cre_dir() {
@@ -25,7 +28,7 @@ function cre_dir() {
   }
 }
 
-router.post("/bgImageRemoval", upload.any("files"), async (req, res) => {
+router.post("/compressImageSize", upload.any("files"), async (req, res) => {
   if (!req.files || req.files.length === 0) {
     return res.status(400).send("No files uploaded");
   }
@@ -36,20 +39,26 @@ router.post("/bgImageRemoval", upload.any("files"), async (req, res) => {
 
   try {
     cre_dir();
-    console.log(req.files.length, req.files);    
-    const outputDir = req.files.length === 1 ? "Download/bgImageRemoval/" : "Download/temp_pdf/";
-
+    console.log(req.files.length,req.body.quality, req.files);
+    const outputDir = req.files.length === 1 ? main_dir[1] : main_dir[0];
+    
+    let compressionoption={
+      quality:Number(req.body.quality) || 50,
+      progressive : true ,
+      force : false
+    }
     for (const file of req.files) {
-      const fileName = `flexxpdf_bgImageRemoval_${path.parse(file.originalname).name}_${Date.now()}.png`;
+      const fileName = `flexxpdf_compressImage_${path.parse(file.originalname).name}_${Date.now()}.jpeg`;
       const outDir = path.join(outputDir, fileName);
 
-      let cmd = `rembg i "${file.path}" "${outDir}"`;       
-      await execPromise(cmd);
+      await sharp(file.path)
+      .jpeg(compressionoption)
+      .toFile(outDir)
       processedFiles.push(outDir);
     }
 
     if (req.files.length > 1) {
-      zipPath = path.join("Download/bgImageRemoval/", `flexxpdf_bgImageRemoval_${Date.now()}.zip`);
+      zipPath = path.join(main_dir[1], `flexxpdf_compressImage_${Date.now()}.zip`);
       await createZipFile(processedFiles, zipPath, "files");
       finalDownloadPath = zipPath;
     } else {
@@ -61,9 +70,53 @@ router.post("/bgImageRemoval", upload.any("files"), async (req, res) => {
     });
 
   } catch (error) {
-    console.error("Background Removal Error:", error);
+    console.error("Compress image Error:", error);
     cleanupFiles(processedFiles, req.files);
-    res.status(500).send("Background removal failed");
+    res.status(500).send("Compress image failed");
+  }
+});
+
+router.post("/imageFormatConverter", upload.any("files"), async (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).send("No files uploaded");
+  }
+
+  let processedFiles = [];
+  let zipPath = "";
+  let finalDownloadPath = "";
+  let convertFileType = req.body.format;
+
+  try {
+    cre_dir();
+    console.log(req.files.length,req.body.format ,req.files);
+    const outputDir = req.files.length === 1 ? main_dir[2] : main_dir[0];
+    
+    for (const file of req.files) {
+      const fileName = `flexxpdf_imageFormatConverter_${path.parse(file.originalname).name}_${Date.now()}.${convertFileType}`;
+      const outDir = path.join(outputDir, fileName);
+
+      await sharp(file.path)
+      .toFormat(convertFileType , {quality:100})
+      .toFile(outDir)
+      processedFiles.push(outDir);
+    }
+
+    if (req.files.length > 1) {
+      zipPath = path.join(main_dir[2], `flexxpdf_imageFormatConverter_${Date.now()}.zip`);
+      await createZipFile(processedFiles, zipPath, "files");
+      finalDownloadPath = zipPath;
+    } else {
+      finalDownloadPath = processedFiles[0];
+    }
+
+    res.download(finalDownloadPath, (err) => {
+      cleanupFiles([zipPath, ...processedFiles], req.files);
+    });
+
+  } catch (error) {
+    console.error("image Format Converter Error:", error);
+    cleanupFiles(processedFiles, req.files);
+    res.status(500).send("image Format Converter failed");
   }
 });
 
