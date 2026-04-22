125fc6b1cb8ddee5e7b88bbd30472d163f2f2169
Akshat Patel <128491185+Akshatpatel004@users.noreply.github.com>
2026-04-22 08:44:57 +0530

    Add files via upload

--- PDF-TOOLS_Telegram-bot/firebase-setup.js
+++ PDF-TOOLS_Telegram-bot/firebase-setup.js
@@ -1,10 +1,26 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+try {
+    if (!process.env.FIREBASE_KEY) {
+        throw new Error ("Firebase key is missing from environment variable");
+    }
 
-admin.initializeApp({
-    credential : admin.credential.cert(serviceAccount)
-});
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+    if (serviceAccount.private_key) {
+        serviceAccount.private_key = serviceAccount.private_key.replace(/\ \n/g, '\n');
+    }
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    console.log("Firebase admin initialized successfully"); 
+
+} catch (error) {
+    console.log("Firebase admin initialized error", error); 
+    return null;
+}
 
 const firestoreDB = admin.firestore();
 
